5d9a032e0b28b2d41c5d5656b84c569f2609db03
Erik Carlsson <info@code301.com>
2015-01-11 19:34:14 +0100

    Init

---
+++ lib/Pod/Elemental/Transformer/Pinter.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Pod::Elemental::Transformer::Pinter;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Pod::Elemental::Transformer::Pinter;
+
+=head1 DESCRIPTION
+
+Pod::Elemental::Transformer::Pinter is ...
+
+=head1 SEE ALSO
+
+=cut
