8bca47a7bf7a75ee34dfcf2ca4301d34a33bc43d
Erik Carlsson <info@code301.com>
2016-01-12 21:57:44 +0000

    Introduce the 'for' attribute (specify format)

--- lib/Pod/Elemental/Transformer/Splint/MethodRenderer.pm
+++ lib/Pod/Elemental/Transformer/Splint/MethodRenderer.pm
@@ -9,9 +9,15 @@ package Pod::Elemental::Transformer::Splint::MethodRenderer;
 
 use Moose::Role;
 use Pod::Simple::XHTML;
+use Types::Standard qw/Str/;
 
 with 'Pod::Elemental::Transformer::Splint::Util';
 requires 'render_method';
 
+has for => (
+    is => 'ro',
+    isa => Str,
+    required => 1,
+);
 
 1;
